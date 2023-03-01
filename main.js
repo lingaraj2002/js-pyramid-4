fdee3ebc5baa4ecada7f90580cf5b7a42af50461
LINGARAJ <lingaraj01052002@gmail.com>
2023-03-01 16:59:48 +0530

    adding.

---
+++ main.js
@@ -0,0 +1,11 @@
+var alpha;		
+			for(var i=1; i<=5; i++){
+				 alpha=65;
+				 for(var j=1; j<=i; j++){
+					 document.write(String.fromCharCode(alpha) + " ");
+					 alpha++;
+					 }
+				 document.write("<br>");
+				 }
+				 
+				  document.write("<br>");
